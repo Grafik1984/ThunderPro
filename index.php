b539ffd5d881be49696335d2deab8dae01a9bba1
lur9622 <mateusz.prendki@gmail.com>
2014-12-12 23:10:53 +0100

    Nowe zmiany

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+      <link rel="stylesheet" href="css/styl.css" type="text/css" />
+  </head>
+  <body>
+    <div id ="content">
+      <nav><div class="option">Testowa1</div><div class="option">Testowa1</div><div class="option">Testowa1</div></nav>
+      <div id="wiersz">
+        <pre>
+          Mama ma kota
+          kot ma ale
+          kupa kupa
+          klocek klocek
+        </pre>
+      </div>
+    </div>
+  </body>
+</html>
